efade09281273546ceac6a9fe455c699deaea000
aviyan <abhiyanlimbu2@gmail.com>
2026-05-30 01:52:15 +0545

    eta uta vako :wp

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-const tailwindcss = require('tailwindcss/lib/plugin.js');
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-    plugins: [
-        tailwindcss(),
-        autoprefixer(),
-    ],
-};
