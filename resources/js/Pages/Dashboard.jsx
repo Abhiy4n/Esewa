a9a0b2bbfbc89c74b2692e9cee6b4090c10f8dc8
Gaurav-mgr <gauravpulami5@gmail.com>
2026-05-30 01:26:18 +0545

    this is newly added

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -23,7 +23,7 @@ export default function Dashboard() {
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                             You're logged in!
-                        </div>
+                        </div> 
                     </div>
                 </div>
             </div>
